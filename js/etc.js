3285042daad7f7cec077b706b86d14a3fc4f6972
8bitbug <137716553+8bitbug@users.noreply.github.com>
2024-02-10 22:06:25 +0100

    dedede

---
+++ js/etc.js
@@ -0,0 +1,14 @@
+/*This file is used for making the ui responsive*/
+
+let settingsButton = document.getElementById('settings');
+let statsButton = document.getElementById('stats');
+let infoButton = document.getElementById('info');
+let prestigeButton = document.getElementById('prestige');
+
+function opensettings() {
+    
+};
+
+settingsButton.addEventListener("click", function() {
+    opensettings();
+})
