d29f23a0cab9a0d2dd8272828e4753ae6b7f7203
8bitbug <137716553+8bitbug@users.noreply.github.com>
2024-02-11 13:42:49 +0000

    updateeee

--- js/etc.js
+++ js/etc.js
@@ -1,14 +1,34 @@
 /*This file is used for making the ui responsive*/
 
-let settingsButton = document.getElementById('settings');
-let statsButton = document.getElementById('stats');
-let infoButton = document.getElementById('info');
-let prestigeButton = document.getElementById('prestige');
+let settingsButton = document.getElementById("settings");
+let statsButton = document.getElementById("stats");
+let infoButton = document.getElementById("info");
+let prestigeButton = document.getElementById("prestige");
+
+let saveButton = document.getElementById("saveButton");
+
+mainclickbutton.addEventListener("mouseover", function () {
+  mainclickbutton.style.transform = "scale(1.1)";
+});
+
+mainclickbutton.addEventListener("mouseout", function () {
+  mainclickbutton.style.transform = "scale(1)";
+});
 
 function opensettings() {
-    
-};
+  if (document.getElementById("settingscontent").style.display == "flex") {
+    document.getElementById("settingscontent").style.display = "none";
+    document.getElementById("upgrades").style.display = "flex";
+  } else {
+    document.getElementById("upgrades").style.display = "none";
+    document.getElementById("settingscontent").style.display = "flex";
+  }
+}
 
-settingsButton.addEventListener("click", function() {
-    opensettings();
-})
+settingsButton.addEventListener("click", function () {
+  opensettings();
+});
+
+saveButton.addEventListener("click", function () {
+  save();
+});
--- js/math.js
+++ js/math.js
@@ -1,76 +1,125 @@
-let mainclickbutton = document.getElementById('mainclickbutton');
-let displayclick = document.getElementById('displayclick');
+let mainclickbutton = document.getElementById("mainclickbutton");
+let displayclick = document.getElementById("displayclick");
 
-let autoclickerWorth = parseInt(localStorage.getItem('autoclickerWorth')) || 100;
-let autoclickerAmount = parseInt(localStorage.getItem('autoclickerAmount')) || 0;
-let autoclickerproduction = parseInt(localStorage.getItem('autoclickerproduction')) || 1;
+let autoclickerWorth =
+  parseInt(localStorage.getItem("autoclickerWorth")) || 100;
+let autoclickerAmount =
+  parseInt(localStorage.getItem("autoclickerAmount")) || 0;
+let autoclickerproduction =
+  parseInt(localStorage.getItem("autoclickerproduction")) || 1;
 let autoclickerinterval;
 
-let autoclickeritem = document.getElementById('autoclicker');
-let autoclickerAmountdis = document.getElementById('autoclickeramount');
-let autoclickerWorthdis = document.getElementById('autoclickercost');
+let autoclickeritem = document.getElementById("autoclicker");
+let autoclickerAmountdis = document.getElementById("autoclickeramount");
+let autoclickerWorthdis = document.getElementById("autoclickercost");
 
-let click = parseInt(localStorage.getItem('click')) || 0;
-let clickRate = parseInt(localStorage.getItem('clickRate')) || 1;
+let clickupgrade = document.getElementById("clickupgrade");
+let clickupgradeifbought = false;
+
+let click = parseInt(localStorage.getItem("click")) || 0;
+let clickRate = parseInt(localStorage.getItem("clickRate")) || 1;
+
+function formatNumber(number) {
+  //Will proably become the biggest function
+  if (number >= 1e3 && number < 1e6) {
+    return (number / 1e3).toFixed(1) + " Thousand";
+  } else if (number >= 1e6 && number < 1e12) {
+    return (number / 1e6).toFixed(1) + " Million";
+  } else if (number >= 1e9 && number < 1e12) {
+    return (number / 1e9).toFixed(1) + " Billoin";
+  } else if (number >= 1e12) {
+    return (number / 1e12).toFixed(1) + " Trillion";
+  }
+}
 
 for (i = 0; i < autoclickerAmount; i++) {
-    setTimeout(() => {
-        autoclickerproducing();
-    }, 100 * i)
-};
+  setTimeout(() => {
+    autoclickerproducing();
+  }, 100 * i);
+}
 
 function displayclicks() {
-    displayclick.innerHTML = click + ' ' + 'clicks';
+  displayclick.innerHTML = formatNumber(click) + " " + "clicks";
 }
 
 displayclicks();
 
-mainclickbutton.addEventListener("click", function() {
-    click = click + clickRate;
-    displayclicks();
-})
+mainclickbutton.addEventListener("click", function () {
+  click = click + clickRate;
+  displayclicks();
+});
+
+function save() {
+  localStorage.setItem("click", click);
+  localStorage.setItem("clickRate", clickRate);
+  localStorage.setItem("autoclickerWorth", autoclickerWorth);
+  localStorage.setItem("autoclickerAmount", autoclickerAmount);
+  localStorage.setItem("autoclickerproduction", autoclickerproduction);
+  localStorage.setItem("clickupgradeifbought", clickupgradeifbought);
+}
 
 setInterval(() => {
-    localStorage.setItem('click', click);
-    localStorage.setItem('clickRate', clickRate);
-    localStorage.setItem('autoclickerWorth', autoclickerWorth);
-    localStorage.setItem('autoclickerAmount', autoclickerAmount);
-    localStorage.setItem('autoclickerproduction', autoclickerproduction);
-}, 1)
+  save();
+}, 15000);
 
 function displayautoclicker() {
-    autoclickerAmountdis.innerHTML = autoclickerAmount;
-    autoclickerWorthdis.innerHTML = autoclickerWorth + ' ' + 'clicks';
+  autoclickerAmountdis.innerHTML = autoclickerAmount;
+  autoclickerWorthdis.innerHTML =
+    formatNumber(autoclickerWorth) + " " + "clicks";
 }
 
 displayautoclicker();
 
 function autoclickerproducing() {
-    autoclickerinterval = setInterval(() => {
-        click += autoclickerproduction;
-        displayclicks();
-    }, 1000)
+  autoclickerinterval = setInterval(() => {
+    click += autoclickerproduction;
+    displayclicks();
+  }, 1000);
 }
 
 function autoclickerbuy() {
-    if (click >= autoclickerWorth) {
-        click = click - autoclickerWorth;
-        autoclickerAmount = autoclickerAmount + 1;
-        autoclickerWorth = autoclickerWorth * (1 + 0.16);
-        autoclickerWorth = Math.floor(autoclickerWorth);
-        setTimeout(() => {
-            autoclickerproducing()
-        }, 100 * i)
-        displayautoclicker();
-        displayclick();
-    };
-};
+  if (click >= autoclickerWorth) {
+    click = click - autoclickerWorth;
+    autoclickerAmount = autoclickerAmount + 1;
+    autoclickerWorth = autoclickerWorth * (1 + 0.16);
+    autoclickerWorth = Math.floor(autoclickerWorth);
+    setTimeout(() => {
+      autoclickerproducing();
+    }, 100 * i);
+    displayautoclicker();
+    displayclick();
+  }
+}
 
-autoclickeritem.addEventListener("click", function() {
-    autoclickerbuy();
-})
+autoclickeritem.addEventListener("click", function () {
+  autoclickerbuy();
+});
 
 setInterval(() => {
-    displayclicks()
-    displayautoclicker()
-}, 1)
+  displayclicks();
+  displayautoclicker();
+}, 1);
+
+function clickupgradebuy() {
+  if (click >= 250) {
+    click = click - 250;
+    clickRate = clickRate * 2;
+    clickupgradeifbought = true;
+    clickupgrade.style.display = "none";
+    displayclicks();
+  }
+}
+
+clickupgrade.addEventListener("click", function () {
+  clickupgradebuy();
+});
+
+clickupgradeifbought = Boolean(localStorage.getItem("clickupgradeifbought"));
+
+setInterval(() => {
+  if (clickupgradeifbought == true) {
+    clickupgrade.style.display = "none";
+  } else if (clickupgradeifbought == false) {
+    clickupgrade.style.display = "flex";
+  }
+}, 1);
